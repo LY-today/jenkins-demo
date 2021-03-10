204a312ee0b2340840d9719e962c15c100c73574
LY-today <33409445+LY-today@users.noreply.github.com>
2021-03-10 18:42:54 +0800

    Update main.go

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 func main() {
-	fmt.Println("BRANCH:",os.Getenv("branch"))
+	fmt.Println("BRANCH_NAME:", os.Getenv("branch"))
 	http.HandleFunc("/", HelloHandler)
 	err := http.ListenAndServe(":8001", nil)
 	fmt.Println(err)
