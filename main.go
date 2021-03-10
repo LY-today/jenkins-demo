d00f541bf3a330eac6d1aa66a59ff3575af8a86f
liyang <724102053@qq.com>
2021-03-10 20:19:01 +0800

    change

--- main.go
+++ main.go
@@ -10,7 +10,7 @@ func HelloHandler(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprintf(w, "Hello World")
 }
 func main() {
-	fmt.Println("BRANCH_NAME:", os.Getenv("branch"))
+	fmt.Println("BRANCH_NAME1:", os.Getenv("branch"))
 	fmt.Println("启动1")
 	http.HandleFunc("/", HelloHandler)
 	err := http.ListenAndServe(":8001", nil)
